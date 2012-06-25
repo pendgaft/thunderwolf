5c788c5c5eaaa887e5fddb8f605affcfcb5689b6
Max Schuchard <schuch@cs.umn.edu>
2012-06-25 14:28:07 -0500

    turned off debug

--- src/sim/ThunderWolf.java
+++ src/sim/ThunderWolf.java
@@ -9,7 +9,7 @@ import threading.BGPMaster;
 
 public class ThunderWolf {
 
-	public static final boolean DEBUG = true;
+	public static final boolean DEBUG = false;
 
 	public static void main(String[] args) throws IOException {
 
