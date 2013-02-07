b9e29af2885acdc439113339e029bee40622f98f
pendgaft <schuch@cs.umn.edu>
2013-02-07 11:28:22 -0600

    refactored ThunderWolf.java so it's not just one giant main function, and is actually a Java style class

--- src/sim/ThunderWolf.java
+++ src/sim/ThunderWolf.java
@@ -11,10 +11,54 @@ import threading.BGPMaster;
 public class ThunderWolf {
 
 	public static final boolean DEBUG = false;
+	
+	/**
+	 * This flag turns on a somewhat ghetto hack when dealing with our
+	 * relationship to "ourself"
+	 */
+	//TODO look into what this does deeply, as I don't fucking like it
 	public static final boolean FASTANDLOOSE = true;
+
+	protected HashMap<Integer, BGPSpeaker> routerMap = null;
+	private NetworkSeeder netSeeder = null;
+
+	protected enum Mode {
+		EVEN, INJECTOR
+	}
 
 	public static void main(String[] args) throws IOException {
 
+		/*
+		 * Determine mode type
+		 */
+		Mode theMode;
+		if (args[1].equalsIgnoreCase("even")) {
+			theMode = Mode.EVEN;
+		} else if (args[1].equalsIgnoreCase("injector")) {
+			theMode = Mode.INJECTOR;
+		} else {
+			System.err.println("Bad mode given: " + args[1]);
+			return;
+		}
+
+		/*
+		 * Build the ThunderWolf Object and run the simulation
+		 */
+		ThunderWolf me = new ThunderWolf(args[0], theMode);
+		me.runSimulation();
+	}
+
+	private ThunderWolf() {
+		/*
+		 * Hidden since default constructor makes no sense
+		 */
+	}
+
+	protected ThunderWolf(String topologyFile, Mode myMode) throws IOException {
+
+		/*
+		 * Longs used to store wall clock times for speed reporting
+		 */
 		long start, end;
 
 		/*
@@ -22,44 +66,27 @@ public class ThunderWolf {
 		 */
 		System.out.println("Creating router topology.");
 		start = System.currentTimeMillis();
-		HashMap<Integer, BGPSpeaker> routerMap = ASTopoParser.doNetworkBuild(args[0]);
+		this.routerMap = ASTopoParser.doNetworkBuild(topologyFile);
 		end = System.currentTimeMillis();
 		System.out.println("Topology created in: " + (end - start) / 1000
 				+ " seconds.\n");
 
 		/*
-		 * Debug, dump topo info
-		 */
-		if (DEBUG) {
-			System.out.println("Topo size is: " + routerMap.size());
-			for (BGPSpeaker tRouter : routerMap.values()) {
-				System.out.println(tRouter.printDebugString());
-			}
-		}
-		
-		/*
 		 * Setup network seeder
 		 */
-		NetworkSeeder netSeed = null;
-		//TODO factor this code off to a function for cleanlyness
-		if(args[1].equalsIgnoreCase("even")){
-			netSeed = new EvenSeed(routerMap);
-		} else if(args[1].equalsIgnoreCase("injector")){
-			netSeed = new SingleInjector(routerMap, 1, 350000, 10);
-		} else{
-			System.err.println("Bad mode given: " + args[1]);
-			return;
-		}
-		
-		System.out.println("Firing Sim Trigger");
-		BGPMaster.driveSim(routerMap, netSeed);
-
-		if (DEBUG) {
-			System.out
-					.println("Tables look like..........\n*******************");
-			for (BGPSpeaker tRouter : routerMap.values()) {
-				System.out.println(tRouter.printBGPString(true));
-			}
+		if (myMode == Mode.EVEN) {
+			this.netSeeder = new EvenSeed(routerMap);
+		} else if (myMode == Mode.INJECTOR) {
+			this.netSeeder = new SingleInjector(routerMap, 1, 350000, 10);
+		} else {
+			throw new RuntimeException(
+					"Passed a mode to ThunderWolf Constructor that we do not know how to seed!");
 		}
 	}
+
+	protected void runSimulation() throws IOException {
+		System.out.println("Firing Sim Trigger");
+		BGPMaster.driveSim(this.routerMap, this.netSeeder);
+	}
+
 }
