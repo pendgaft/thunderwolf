de3cfec0f21ea06083e39d85ab05f50b3807304c
Max Schuchard <schuch@cs.umn.edu>
2013-05-10 11:57:38 -0500

    yet more tweaks to building weighted IP files

--- src/topoCreating/FullInternetBuild.java
+++ src/topoCreating/FullInternetBuild.java
@@ -7,40 +7,46 @@ import router.*;
 
 public class FullInternetBuild {
 
-	
 	private static final String BASE_AS_REL_FILE = "/scratch/waterhouse/schuch/asData/current-as-rel";
 	private static final String BASE_IP_FILE = "/scratch/waterhouse/schuch/asData/current-ip-count";
-	private static final String OUT_FILE = "whole-internet";
-	
+	private static final String OUT_FILE = "pruned-weighted-internet";
+
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) throws IOException{
-		
+	public static void main(String[] args) throws IOException {
+
 		ASTopoParser topParse = new ASTopoParser(FullInternetBuild.BASE_AS_REL_FILE, null, false);
-		HashMap<Integer, BGPSpeaker> startingMap = topParse.doNetworkBuild(false);
+		HashMap<Integer, BGPSpeaker> startingMap = topParse.doNetworkBuild(true);
 		System.out.println("original parse: " + topParse.getUnpruned().size());
-		HashMap<Integer, AS> adjusted = FullInternetBuild.incorpIPCount(BASE_IP_FILE, topParse.getUnpruned());
-		System.out.println("sent back: " + adjusted.size());
-		
+		System.out.println("pruned size: " + startingMap.keySet().size());
+		HashMap<Integer, AS> adjusted = FullInternetBuild.incorpIPCount(BASE_IP_FILE, topParse.getUnpruned(),
+				startingMap);
+		System.out.println("Adjusted size: " + adjusted.size());
+		int sum = 0;
+		for(AS tAS: adjusted.values()){
+			sum += tAS.getCIDRSize();
+		}
+		System.out.println("total ip count: " + sum);
+
 		//HashMap<Integer, AS> asMap = new HashMap<Integer, AS>();
 		//for(int tASN: startingMap.keySet()){
 		//	asMap.put(tASN, startingMap.get(tASN).getASObject());
 		//}
-		
 		//HashMap<Integer, AS> prunedMap = FullInternetBuild.pruneTopo(asMap);
 		//HashMap<Integer, AS> secondPruneMap = FullInternetBuild.pruneTopo(prunedMap);
 		FullInternetBuild.dumpToFile(adjusted);
 	}
-	
-	private static void dumpToFile(HashMap<Integer, AS> asMap) throws IOException{
+
+	private static void dumpToFile(HashMap<Integer, AS> asMap) throws IOException {
 		BufferedWriter outFile = new BufferedWriter(new FileWriter(FullInternetBuild.OUT_FILE + "-rel.txt"));
-		
+
 		long count = 0;
-		for(AS tAS: asMap.values()){
-			for(int tNeighbor: tAS.getNeighbors()){
+		for (AS tAS : asMap.values()) {
+			for (int tNeighbor : tAS.getNeighbors()) {
 				/*
-				 * THE SIGN FLIP IS CORRECT.  getRel returns their relationship to us, we want our relationship to them!!!!
+				 * THE SIGN FLIP IS CORRECT. getRel returns their relationship
+				 * to us, we want our relationship to them!!!!
 				 */
 				outFile.write("" + tAS.getASN() + "|" + tNeighbor + "|" + (tAS.getRel(tNeighbor) * -1) + "\n");
 				count++;
@@ -48,115 +54,95 @@ public class FullInternetBuild {
 		}
 		outFile.close();
 		System.out.println("line count: " + count);
-		
+
 		outFile = new BufferedWriter(new FileWriter(FullInternetBuild.OUT_FILE + "-ip.txt"));
-		for(AS tAS: asMap.values()){
+		for (AS tAS : asMap.values()) {
 			outFile.write("" + tAS.getASN() + " " + tAS.getCIDRSize() + "\n");
 		}
 		outFile.close();
-		
+
 	}
 
-	
-	private static HashMap<Integer, AS> pruneTopo(HashMap<Integer, AS> startingMap){
-		HashSet<Integer> toRemove = new HashSet<Integer>();
-		
-		/*cidrCount
-		 * find all the ASNs of the ASes we want to prune
-		 */
-		for(AS tAS: startingMap.values()){
-			if(tAS.getCustomers().size() == 0){
-				toRemove.add(tAS.getASN());
-			}
-		}
-		
-		/*
-		 * create the new AS objects, need all of them done before adding relationships
-		 */
-		HashMap<Integer, AS> retMap = new HashMap<Integer, AS>();
-		for(AS tAS: startingMap.values()){
-			if(toRemove.contains(tAS.getASN())){
-				continue;
-			}
-			
-			AS alteredAS = new AS(tAS.getASN(), tAS.getCIDRSize());
-			retMap.put(alteredAS.getASN(), alteredAS);
-		}
-		
-		/*
-		 * add in the relationships
-		 */
-		for(AS tAS: retMap.values()){
-			AS oldAS = startingMap.get(tAS.getASN());
-			
-			for(int tCust: oldAS.getCustomers()){
-				if(toRemove.contains(tCust)){
-					continue;
-				}void
-				
-				tAS.addCustomer(startingMap.get(tCust));
-			}
-			for(int tPeer: oldAS.getPeers()){
-				if(toRemove.contains(tPeer)){
-					continue;
-				}
-				
-				tAS.addPeer(startingMap.get(tPeer));
-			}
-			for(int tProv: oldAS.getProviders()){
-				tAS.addProvider(startingMap.get(tProv));
-			}
-		}ger> keySet = topo.keySet();
-		keySet.r
-		
-		return retMap;
-	}
-	
-	private static HashMap<Integer, AS> incorpIPCount(String ipCountFile, HashMap<Integer, AS> topo) throws IOException{
+	private static HashMap<Integer, AS> incorpIPCount(String ipCountFile, HashMap<Integer, AS> fullTopo,
+			HashMap<Integer, BGPSpeaker> prunedMap) throws IOException {
 		HashMap<Integer, AS> retMap = new HashMap<Integer, AS>();
 		int noASCount = 0;
 		int totalIPCount = 0;
 		HashSet<Integer> seenASes = new HashSet<Integer>();
-		
+
 		BufferedReader fBuff = new BufferedReader(new FileReader(ipCountFile));
-		while(fBuff.ready()){
+		while (fBuff.ready()) {
 			String line = fBuff.readLine();
 			/*
 			 * Ignore comments like a baws
 			 */
-			if(line.startsWith("#")){
+			if (line.startsWith("#")) {
 				continue;
 			}
-			
+
 			StringTokenizer tokens = new StringTokenizer(line, ",");
 			int asn = Integer.parseInt(tokens.nextToken());
-			int ipCount = Integer.parseInt(tokens.nextToken());			
-			
+			int ipCount = Integer.parseInt(tokens.nextToken());
+
+			/*
+			 * Sanity check that the AS in question actually exists
+			 */
 			totalIPCount += ipCount;
-			if(!topo.containsKey(asn)){
+			if (!fullTopo.containsKey(asn)) {
 				noASCount += ipCount;
 				continue;
 			}
-			
-			AS tAS = topo.get(asn);
+
+			/*
+			 * Pull the AS, and update its CIDR count
+			 */
+			AS tAS = fullTopo.get(asn);
 			tAS.setCIDRSize(ipCount);
 			seenASes.add(asn);
 			retMap.put(tAS.getASN(), tAS);
 		}
-		
-		Set<Integer> keySet = topo.keySet();
+
+		Set<Integer> keySet = fullTopo.keySet();
 		keySet.removeAll(seenASes);
-		
-		for(int tASN: keySet){
-			retMap.put(tASN, topo.get(tASN));
+
+		/*
+		 * Copy over all ASes that we didn't see in our file
+		 */
+		for (int tASN : keySet) {
+			retMap.put(tASN, fullTopo.get(tASN));
 		}
-		
+
+		/*
+		 * Dump some stats
+		 */
 		System.out.println("IP Integration Summary:");
 		System.out.println("Total IP blocks: " + totalIPCount);
 		System.out.println("Assigned IP blocks: " + (totalIPCount - noASCount));
 		System.out.println("Un-assigned IP blocks: " + noASCount);
 		System.out.println("Non adjusted ASes: " + keySet.size());
-		
-		return retMap;
+
+		/*
+		 * Now merge pruned ASes since it looks like we can't do this the simple
+		 * way because of memory issues, first add all existing ASes, then fold
+		 * in the pruned ASes to their providers
+		 */
+		HashMap<Integer, AS> finalPrunedMap = new HashMap<Integer, AS>();
+		for (int tASN : prunedMap.keySet()) {
+			AS tAS = prunedMap.get(tASN).getASObject();
+			tAS.setCIDRSize(retMap.get(tASN).getCIDRSize());
+			finalPrunedMap.put(tASN, tAS);
+		}
+		for(int tASN: retMap.keySet()){
+			if(finalPrunedMap.containsKey(tASN)){
+				continue;
+			}
+			
+			AS tAS = retMap.get(tASN);
+			for(int tProv: tAS.getProviders()){
+				finalPrunedMap.get(tProv).setCIDRSize(finalPrunedMap.get(tProv).getCIDRSize() + tAS.getCIDRSize());
+			}
+		}
+
+		return finalPrunedMap;
 	}
 }
