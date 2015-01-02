5d720baf5991c14313b1bc9a80ce4caa20b82e1a
pendgaft <m.schuchard@gmail.com>
2015-01-02 15:47:36 -0600

    added event logging instead of having special purpose code in the simulator to handle it

---
+++ src/events/LoggingEvent.java
@@ -0,0 +1,27 @@
+package events;
+
+import java.io.IOException;
+
+import logging.SimLogger;
+
+public class LoggingEvent extends SimEvent {
+
+	public LoggingEvent(long time) {
+		super(time, SimEvent.LOGGING_EVENT, null);
+	}
+
+	@Override
+	public void handleEvent(SimLogger theLogger) {
+		try {
+			theLogger.processLogging();
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(-2);
+		}
+	}
+
+	public SimEvent repopulate() {
+		return new LoggingEvent(this.getEventTime() + SimLogger.LOG_EPOCH);
+	}
+
+}
